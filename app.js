973c15046b171c66606ee237a8f26deea99219af
Lalit <lalit.garghate@gmail.com>
2021-09-25 23:21:17 +0530

    parsing req data

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html><body>Hi lalit</body></html>");
-  res.end();
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write(
+      "<html><body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>send</button></form></body></html>"
+    );
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
 });
 
 server.listen(3000);
