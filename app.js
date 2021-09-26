18cf039f6b86f9b94968cadd574a512b64dd302a
Lalit <lalit.garghate@gmail.com>
2021-09-26 22:24:52 +0530

    template engines

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const http = require("http");
-const routes = require("./routes");
+const path = require("path");
 
-const server = http.createServer(routes);
+const express = require("express");
+const bodyParser = require("body-parser");
 
-server.listen(3000);
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.router);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(400).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(3000);
